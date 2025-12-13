5f1e25fdd545f5b88393520810faf2118ead677f
br14nn <vituallabrian727@gmail.com>
2025-12-13 18:33:50 +0800

    chore: installed & setup swagger ui

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder().setTitle('Backend').build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, documentFactory);
+
+  app.setGlobalPrefix('api');
+  await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
