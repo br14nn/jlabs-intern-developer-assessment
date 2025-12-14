db051105adab0b13172e5f64d1a6b7b7160f7f72
br14nn <vituallabrian727@gmail.com>
2025-12-15 03:28:30 +0800

    BREAKING CHANGE: reset backend API problem running with prismORM

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const config = new DocumentBuilder().setTitle('Backend').build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('', app, documentFactory);
-
-  app.setGlobalPrefix('api');
-  await app.listen(process.env.PORT ?? 8000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
