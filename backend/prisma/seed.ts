9b3d2876890d2ca29c747af7fb351207366243c2
br14nn <vituallabrian727@gmail.com>
2025-12-15 19:26:53 +0800

    feat: added DB seeding

---
+++ backend/prisma/seed.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from 'src/generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+const prisma = new PrismaClient({ adapter });
+
+async function main() {
+  await prisma.user.upsert({
+    where: {
+      id: 1,
+    },
+    create: {
+      id: 1,
+      username: 'root',
+      password: '1234',
+    },
+    update: {},
+  });
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
