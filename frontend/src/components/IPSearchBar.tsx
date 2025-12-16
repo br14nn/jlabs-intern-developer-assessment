0ad8d517ec39159bfc8597fc75b02b255025ab2d
br14nn <vituallabrian727@gmail.com>
2025-12-16 23:24:29 +0800

    feat: added ip search bar component

---
+++ frontend/src/components/IPSearchBar.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+
+interface IPSearchBarProps {
+  onSubmit?: React.FormEventHandler<HTMLFormElement>;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+  value?: string;
+}
+
+const IPSearchBar = ({ onSubmit, onChange, value }: IPSearchBarProps) => {
+  return (
+    <form
+      className="flex w-full max-w-80 flex-col gap-2 p-4 md:max-w-140 md:flex-row"
+      onSubmit={onSubmit}
+    >
+      <Input
+        className="h-10 bg-white selection:bg-neutral-600 focus-visible:ring-0"
+        type="text"
+        placeholder="Input IP Address"
+        onChange={onChange}
+        value={value}
+      />
+      <Button variant="secondary" size="lg">
+        Enter IP
+      </Button>
+    </form>
+  );
+};
+
+export default IPSearchBar;
